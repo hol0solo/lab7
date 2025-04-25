44319ab1ed7c58feef90ea4ba5cd7dce15fbf524
Nikita <88550141+hol0solo@users.noreply.github.com>
2025-04-25 20:41:09 +0500

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -37,10 +37,17 @@ class OrdersChecker:
         if self.path_to_json:
             with open(self.path_to_json, 'r', encoding='utf-8') as file:
                 response_js = json.load(file)
-                filled_orders_data = [{"OrderID": elem["orderID"], "price": float(elem["price"]), "quantity": int(elem["quantity"])}
-                    for index, elem in enumerate(response_js["result"]) if response_js["result"][index]["status"] == "filled"]
+                if "price" in response_js[0].keys():
+                    filled_orders_data = [{"OrderID": elem["order_id"], "price": float(elem["price"]), "quantity": int(elem["quantity"])}
+                        for index, elem in enumerate(response_js) if response_js[index]["status"] == "filled"]
+                    print("Файл есть все исправно")
+                    return filled_orders_data
+                else:
+                    orders_ids = [elem["order_id"] for elem in response_js if elem["status"] == "filled"]
+                    print("Файл есть но цены в нем нет идем искать цену")
+                    filled_orders_data = await self.each_order_check(orders_ids)
+                    return filled_orders_data
 
-                return filled_orders_data
 
         url = f"{self.base_url}/orders/history"
         async with self.session.get(url=url, headers=self.headers, params=self.payload) as response:
@@ -50,6 +57,22 @@ class OrdersChecker:
                                       for index, elem in enumerate(response_js["result"])
                                     if response_js["result"][index]["status"] == "filled"]
                 return filled_orders_data
+
+
+    async def each_order_check(self, orders_ids: list):
+        """
+            Функция для сбора информации о точном ордере на бирже
+        """
+        filled_orders_data = []
+        for order_id in orders_ids:
+            url = f"{self.base_url}/orders/{order_id}"
+            async with self.session.get(url=url, headers=self.headers, params={"orderID": order_id}) as response:
+                if response.status == 200:
+                    response_js = await response.json()
+                    filled_order_data = {"OrderID": response_js["result"]["orderID"],
+                                          "price": float(response_js["result"]["price"]), "quantity": int(response_js["result"]["quantity"])}
+                    filled_orders_data.append(filled_order_data)
+        return filled_orders_data
 
 
     async def post_orders(self):
@@ -80,15 +103,16 @@ class OrdersChecker:
                     print("Замечено отклонение от рыночной цены более чем на 10% Ордер не может выполниться")
 
 
-
 async def main():
     """
         Запусти меня!
     """
     async with aiohttp.ClientSession() as session:
         absum = OrdersChecker(session, os.path.abspath("myjsOrders.json"), "1INCH/USDT")
-        await absum.history_orders_check()
-
+        result = await absum.history_orders_check()
+        print(json.dumps(result, indent=4))
+        with open("result.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(result, indent=4))
 
 if __name__ == "__main__":
     asyncio.run(main())
